2cc6b44d89d8c155d24dfd807fc4329ebe41cb1e
Anna Jachym <ania.jachym@gmail.com>
2018-12-15 10:25:48 +0100

    Add animation for the marker

--- src/Map.js
+++ src/Map.js
@@ -11,22 +11,19 @@ class Map extends React.Component {
 
 
           initMap = () => {
-            var map;
+            let map;
             const {google} = this.props;
             const maps = google.maps;
-
-
-            map = new maps.Map(document.getElementById("map"), {
-                 center:{
-                  lat: 51.107883,
-                  lng: 17.038538
-                 },
-                 zoom: 7,
-              });
+                  map = new maps.Map(document.getElementById("map"), {
+                       center:{
+                        lat: 51.107883,
+                        lng: 17.038538
+                       },
+                       zoom: 7,
+                    });
 
 
             let markers = [];
-
             let contents = [];
             let bounds = new window.google.maps.LatLngBounds();
             let infowindows = new google.maps.InfoWindow({
@@ -34,17 +31,17 @@ class Map extends React.Component {
                 maxWidth: 300
             });
 
-
+            // locations for markers
             let  places =
                      [
                       {name: "Łódź",        location:{lat:51.759445,  lng:19.457216}},
-                      {name: "Wroclaw",     location:{lat:51.107883,	 lng:17.038538}},
+                      {name: "Wrocław",     location:{lat:51.107883,	lng:17.038538}},
                       {name: "Katowice",    location:{lat:50.270908,  lng:19.039993}},
                       {name: "Kraków",      location:{lat:50.049683,  lng:19.944544}},
-                      {name: "Warsaw",      location:{lat:52.237049,  lng:21.017532}},
-                      {name: "Ostrava",     location:{lat:49.820923,	 lng:18.262524}},
+                      {name: "Warszawa",    location:{lat:52.237049,  lng:21.017532}},
+                      {name: "Ostrava",     location:{lat:49.820923,	lng:18.262524}},
                       {name: "Brno",        location:{lat:49.195061,  lng:16.606836}},
-                      {name: "Poznań",      location:{lat:52.409538,	 lng:16.931992}},
+                      {name: "Poznań",      location:{lat:52.409538,	lng:16.931992}},
                       {name: "Tychy",       location:{lat:50.124981,  lng:19.009438}}
                     ];
 
@@ -52,41 +49,44 @@ class Map extends React.Component {
              for (var i=0; i < places.length; i++){
                 let positionOfPlaces = places[i].location;
                 let name = places[i].name;
+                // create marker for location and put it into markers array
                 let marker = new google.maps.Marker({
                    map: map,
                    position: positionOfPlaces,
+                   animation: google.maps.Animation.DROP,
                    visibile:true,
                    title: name,
                  });
                 markers.push(marker);
-                 marker.index = i; //add index property
-                 
-                 // infowindow content = name of the place
+                marker.index = i; //add index property
+                 // infowindow content = name of the city
                 contents[i] = (`<div>${marker.title}</div>`);
 
-                google.maps.event.addListener(marker, 'click', function() {
-                  if(infowindows.marker === this.marker){
-                    console.log(this.index); // this will give correct index
-                    console.log(i); //this will always give 10 for you
-                    infowindows.open(map, marker);
-                    infowindows.setContent(`<div>${marker.title}</div>`);
-                    map.panTo(markers[this.index].getPosition());
-                } else {
-                  if (this.infowindows.marker === markers[i]){
-                    this.infowindows.close();
-                }
-               }
-
-              })
+                    // loop to set marker infowindow, animation for markers.
+                    google.maps.event.addListener(marker, 'click', function() {
+                      // set marker to bounce 2 times after click
+                      marker.setAnimation(google.maps.Animation.BOUNCE);
+                      setTimeout(function() {
+                         marker.setAnimation(null);
+                      }, 1000);
+                      if (infowindows.marker === this.marker){
+                          console.log(this.index); // this will give correct index
+                          console.log(i); //this will always give 10 for you
+                          infowindows.open(map, marker);
+                          infowindows.setContent(`<div>${marker.title}</div>`);
+                          map.panTo(markers[this.index].getPosition());
+                        // after clicking on other marker infowindow will close
+                      } else {
+                          if (this.infowindows.marker === markers[i]){
+                              this.infowindows.close();
+                          }
+                        }
+                    });
 
                 bounds.extend(marker.position);
                }
                map.fitBounds(bounds);
              };
-
-
-
-
 
 
 
