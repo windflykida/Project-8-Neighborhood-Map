491eb1e50e952fb3e1c516d88d43b000fbae0b90
Anna Jachym <ania.jachym@gmail.com>
2018-12-18 10:29:32 +0100

    Add Footer to the map

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+const Footer = (() => {
+
+	return (
+		<footer id="footer">
+			<p> Created by Anna Jachym </p>
+		</footer>
+	);
+});
+
+export default Footer;
