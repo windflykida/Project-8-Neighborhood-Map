7d322882a1a40950ee675f8a91400a9c81db52b6
Anna Jachym <ania.jachym@gmail.com>
2018-12-18 10:33:00 +0100

    Added cities to the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,33 +1,56 @@
 
 import React from "react";
+import "./App.css";
+import Modal from "react-modal";
+import Menu from "./img/menu-button.svg";
+
+// https://www.npmjs.com/package/react-modal
+
 
 
 class Sidebar extends React.Component {
 
-   toggleSidebar=()=>{
-     document.getElementsByClassName("sidebar-container").classList.toggle("active");
+   constructor(props){
+     super(props);
+
+       this.state = {
+         // sidebar will show after clicking the hamburger icon
+        showModal: true,
+       };
      }
 
+     render(){
 
+       return(
+         <div className =" sidebar-container">
+         <aside id = "sidebar-left">
+           <div className ="list">
 
-
-  render() {
-      return (
-        <aside className="sidebar-container"
-               onClick={this.props.toggleSidebar}
-               tabIndex="0">
+                <ul> Łódź     </ul>
+                <ul> Wrocław  </ul>
+                <ul> Wrocław  </ul>
+                <ul> Katowice </ul>
+                <ul> Kraków   </ul>
+                <ul> Warszawa </ul>
+                <ul> Ostrava  </ul>
+                <ul> Brno     </ul>
+                <ul> Poznań   </ul>
+                <ul> Tychy    </ul>
+          </div>
         </aside>
 
+        <aside id = "sidebar-right">
+
+
+        </aside>
+      </div>
 
 
 
+       )
+     }
+   }
 
 
-
-
-    )
-
-}
-}
 
 export default Sidebar;
