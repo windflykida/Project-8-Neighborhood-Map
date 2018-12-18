80a1f39e829d861f578499a1a89efa5d492551d1
Anna Jachym <ania.jachym@gmail.com>
2018-12-18 13:07:57 +0100

    add onMouseDown prop to hamburger button

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,26 @@
 import React from "react";
-import Menu from "./menu-button.svg";
+import Menu from "./img/menu-button.svg";
+import "./App.css";
 
 // adding menu-button to src made icon visible, but in public folder not.
 
-const Header = (() => {
+class Header extends React.Component{
 
+
+
+render(
+
+){
   return (
+
+
     <header className="header">
-        <img className="header-menu"
+        <img id ="header-menu"
              src={Menu}
              alt="Cities"
              aria-label="Interesting places in Poland and Czech Republic"
              tabIndex="0"
+             onMouseDown={this.props.handleMouseDown}
             />
         <h1 className="header-heading">
             Places worth visiting in Poland and Czech Republic
@@ -19,6 +28,7 @@ const Header = (() => {
 
     </header>
   );
-});
+}
+};
 
 export default Header;
