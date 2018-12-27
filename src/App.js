11cde9c90c0eb91bdfc124dc815f1568ddbda6bb
Anna Jachym <ania.jachym@gmail.com>
2018-12-27 06:25:36 +0100

    add handleClick function with matching list items

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {GoogleApiWrapper} from "google-maps-react";
 import "./App.css";
-import Map from "./Map.js";
-import Sidebar from "./Sidebar.js";
-import Header from "./Header.js";
-import Footer from "./Footer.js";
+import Map from "./Map";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import Footer from "./Footer";
 
 
  class App extends React.Component {
@@ -13,66 +13,85 @@ import Footer from "./Footer.js";
    constructor(props, context){
      super(props, context);
 
-       this.state = {
-         // sidebar will show after clicking the hamburger icon
-        visible: false,
+   this.state = {
+     // sidebar will show after clicking the hamburger icon
+    visible: false,
+    allMarkersFromChild: null
+   };
 
+   this.handleMouseDown = this.handleMouseDown.bind(this);
+   this.toggleMenu = this.toggleMenu.bind(this);
+   //  this.handleClick = this.handleClick.bind(this);
+   }
 
-       };
+    giveMeMarkersMyChild = (markersFromChild) => {
+      this.setState({ allMarkersFromChild: markersFromChild});
+   }
 
-       this.handleMouseDown = this.handleMouseDown.bind(this);
-       this.toggleMenu = this.toggleMenu.bind(this);
+  // function open and close sidebar
+
+     handleMouseDown = (event) => {
+       this.toggleMenu();
+       console.log("clicked");
+       event.stopPropagation();
      }
 
-// function open and close sidebar
+// method responsible for toggling whether visible is true or false.
 
-         handleMouseDown=(event)=>{
+     toggleMenu = () => {
+       this.setState({
+         visible: !this.state.visible
+       });
+     }
+
+    // Znajdź string
+    ZnajdzString = (event) => {
+      console.log("Wywolales funkcję z Sidebar.js na zdarzenie onChange");
+    }
+
+    // https://ourcodeworld.com/articles/read/327/how-to-execute-child-component-function-from-the-parent-component-in-react
+
+    handleClick = (event, city) => {
+       //const { markers } = [];
+       event.preventDefault();
+
+       this.state.allMarkersFromChild.map((marker) => {
+        if (marker.title === city) {
            this.toggleMenu();
-           console.log("clicked");
-           event.stopPropagation();
+           window.google.maps.event.trigger(marker, "click");
          }
+       })
+     }
 
-      // method responsible for toggling whether visible is true or false.
-
-         toggleMenu = () => {
-           this.setState({
-             visible: !this.state.visible
-           });
-         }
-
-// https://ourcodeworld.com/articles/read/327/how-to-execute-child-component-function-from-the-parent-component-in-react
-         hoops = () => {
-           this.refs.child.handleClick();
-         }
 
   render () {
 
     return (
-          <div>
-            <Map
-                google ={this.props.google}
-                handleClick ={this.handleClick}/>
+      <div>
+        <Map
+            google ={this.props.google}
+            handleClick ={this.handleClick}
+            giveMarkersToParent ={this.giveMeMarkersMyChild}/>
+        <Header
+            handleMouseDown={this.handleMouseDown}/>
 
-            <Header
-                handleMouseDown={this.handleMouseDown}/>
+        <Sidebar
+             handleMouseDown={this.handleMouseDown}
+             menuVisibility={this.state.visible}
+             handleClick ={this.handleClick}
+             onChange={this.ZnajdzString}>
 
-            <Sidebar
-                 handleMouseDown={this.handleMouseDown}
+             <div>
+             <Map ref="child"/>
+             </div>
+        </Sidebar>
 
-                 menuVisibility={this.state.visible}
-                 hoops={this.hoops}>
+        <Footer/>
 
-                 <div>
-                 <Map ref="child"/>
-                 </div>
-            </Sidebar>
-
-            <Footer/>
-
-          </div>
-        )
-      }
-    }
+      </div>
+    )
+  }
+}
 
 
 export default GoogleApiWrapper({
