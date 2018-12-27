20dec1aa09b1f260ebce3a6136c331294cf9239e
Anna Jachym <ania.jachym@gmail.com>
2018-12-27 06:27:11 +0100

    add onClick() witch matching events and city

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,19 +3,13 @@ import "./App.css";
 
 
 
-
-
 // https://www.kirupa.com/react/smooth_sliding_menu_react_motion.htm
 
 
 class Sidebar extends React.Component {
 
-
-
-
-     render(){
-
-
+     render()
+     {
 
        let visibility = "hide";
 
@@ -24,37 +18,45 @@ class Sidebar extends React.Component {
         }
 
        return (
+
           <div>
-
-
-
          <div className="sidebar-container"
               tabIndex="0"
               aria-label="sidebar">
            <aside id = "sidebar-left"
-
                   className={visibility}
                   >
-                  <div>
+            <input type ="text"
+                   placeholder ="Search..."
+                   value={this.props.query}
+                    onChange={event => this.props.onChange(event.target.value)}
+                   />
+              <div>
+                    <ul value="Łódź"
+                        onClick={(event) => this.props.handleClick(event, "Łódź")}
+                       > Łódź     </ul>
+                    <ul value="Wrocław"
+                      onClick={(event) => this.props.handleClick(event, "Wrocław")}> Wrocław  </ul>
+                    <ul value="Katowice"
+                      onClick={(event) => this.props.handleClick(event, "Katowice")}> Katowice </ul>
+                    <ul value="Kraków"
+                      onClick={(event) => this.props.handleClick(event, "Kraków")}> Kraków    </ul>
+                    <ul value="Warszawa"
+                      onClick={(event) => this.props.handleClick(event, "Warszawa")}>Warszawa  </ul>
+                    <ul value="Ostrava"
+                      onClick={(event) => this.props.handleClick(event, "Ostrava")}> Ostrava  </ul>
+                    <ul value="Brno"
+                      onClick={(event) => this.props.handleClick(event, "Brno")}>Brno      </ul>
+                    <ul value="Poznań"
+                      onClick={(event) => this.props.handleClick(event, "Poznań")}> Poznań   </ul>
+
+                {/* try to find good functions to connect list with marker */}
+                    <ul value="Tychy"
+                      onClick={(event) => this.props.handleClick(event, "Tychy")}> Tychy    </ul>
 
 
-                        <ul value="Łódź"
-                            onClick={(event) => this.props.hoops(event)}
-                           > Łódź     </ul>
-                        <ul value="Wrocław"> Wrocław  </ul>
-                        <ul value="Katowice"> Katowice </ul>
-                        <ul value="Kraków"> Kraków    </ul>
-                        <ul value="Warszawa">Warszawa  </ul>
-                        <ul value="Ostrava"> Ostrava  </ul>
-                        <ul value="Brno">Brno      </ul>
-                        <ul value="Poznań"> Poznań   </ul>
 
-                    {/* try to find good functions to connect list with marker */}
-                        <ul value="Tychy"> Tychy    </ul>
-
-
-
-                   </div>
+               </div>
           </aside>
 
         <aside id = "sidebar-right">
